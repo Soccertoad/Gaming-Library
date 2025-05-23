51e808a21b8804567ce2e8bd7dc327f9c17983ae
Sophia <111466302+MintyLuve@users.noreply.github.com>
2025-05-23 13:12:32 -0400

    Creates a box :O

---
+++ Snake/App.java
@@ -0,0 +1,17 @@
+package Snake;
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args){
+        // Board
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
