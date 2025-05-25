9b73240ec6b4391dba90fc7ca81d1961a8ace7b9
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-24 23:18:15 -0400

    Setting Up back ground color, introduction further into JFrame

--- PackMan/src/App.java
+++ PackMan/src/App.java
@@ -14,5 +14,8 @@ public class App {
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacManGame = new PacMan();
+        frame.pack();
     }
 }
--- PackMan/src/PacMan.java
+++ PackMan/src/PacMan.java
@@ -1,3 +1,16 @@
-public class PacMan {
-    
+import java.awt.*;
+
+import javax.swing.*;
+
+public class PacMan extends JPanel{
+    private int rowCount = 21;
+    private int columCount = 19;
+    private int tileSize = 32;
+    private int boardWidth = columCount * tileSize;
+    private int boardHeight = rowCount * tileSize;
+
+    PacMan(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLACK);
+    }
 }
