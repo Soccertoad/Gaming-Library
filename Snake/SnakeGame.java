f3caf248c04a5771224cfe6b502093e980685d19
Sophia <111466302+MintyLuve@users.noreply.github.com>
2025-05-24 20:40:35 -0400

    Creates snake body when eat food

--- Snake/SnakeGame.java
+++ Snake/SnakeGame.java
@@ -12,8 +12,11 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     int boardHeight;
     int tileSize = Tile.tileSize;
 
-    // Tiles
+    // Snake
     Tile snakeHead;
+    ArrayList<Tile> snakeBody;
+
+    // Food
     Tile food;
 
     // Game Logic
@@ -29,8 +32,11 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.getHSBColor(0.8f, 0.05f, 0.9f));
         
-        // Tiles
+        // Snake
         snakeHead = new Tile(5, 5);
+        snakeBody = new ArrayList<Tile>();
+
+        // Food
         food = new Tile(0, 0);
         placeFood();
 
@@ -65,9 +71,15 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         g.setColor(Color.getHSBColor(0f, 0.8f, 0.7f));
         g.fillOval(food.getX(), food.getY(), tileSize, tileSize);
 
-        // Snake
+        // Snake Head
         g.setColor(Color.getHSBColor(0.3f, 0.5f, 0.3f));
         g.fillOval(snakeHead.getX(), snakeHead.getY(), tileSize, tileSize);
+
+        // Snake Body
+        for (int i = 0; i<snakeBody.size(); i++){
+            Tile snakePart = snakeBody.get(i);
+            g.fillOval(snakePart.getX(), snakePart.getY(), tileSize, tileSize);
+        }
     }
 
     public void placeFood() {
@@ -75,11 +87,38 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         food.setY((int) ((Math.random() * boardHeight/tileSize) + 1));
     }
 
+    public boolean isColliding(Tile t1, Tile t2){
+        return (t1.getX() == t2.getX()) && (t1.getY() == t2.getY());
+    }
+
     public void moveSnake(){
+        // Eat food
+        if (isColliding(snakeHead, food)){
+            snakeBody.add(new Tile(food.getX()/tileSize, food.getY()/tileSize));
+            placeFood();
+        }
+
+        // Snake Body
+        for (int i = snakeBody.size()-1; i>=0; i--){
+            Tile snakePart = snakeBody.get(i);
+            if (i == 0){
+                snakePart.setX(snakeHead.getX()/tileSize);
+                snakePart.setY(snakeHead.getY()/tileSize);
+            }
+            else {
+                Tile prevSnakePart = snakeBody.get(i-1);
+                snakePart.setX(prevSnakePart.getX()/tileSize);
+                snakePart.setY(prevSnakePart.getY()/tileSize);
+            }
+        }
+
         // Snake Head
         snakeHead.setX(snakeHead.getX()/tileSize + velocityX);
         snakeHead.setY(snakeHead.getY()/tileSize + velocityY);
     }
+
+    
+    /* Implement Methods */
 
     @Override
     public void actionPerformed(ActionEvent e) {
