9d7392bdd869f5065b4541481effbb838d337775
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-26 19:52:15 -0400

    readding changes because git killed itself

--- PackMan/src/PacMan.java
+++ PackMan/src/PacMan.java
@@ -1,8 +1,31 @@
 import java.awt.*;
+import java.util.HashSet;
 
 import javax.swing.*;
 
 public class PacMan extends JPanel{
+
+    class Block{
+        int x;
+        int y;
+        int width;
+        int height;
+        Image image;
+
+        int startX;
+        int startY;
+
+        Block(Image image, int x, int y, int width, int height){
+            this.image = image;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.startX = x;
+            this.startY = y;
+        }
+    }
+
     private int rowCount = 21;
     private int columCount = 19;
     private int tileSize = 32;
@@ -20,6 +43,39 @@ public class PacMan extends JPanel{
     private Image pacmanDownImage;
     private Image pacmanLeftImage;
     private Image pacmanRightImage;
+
+    //Item positions
+    HashSet<Block> walls;
+    HashSet<Block> foods;
+    HashSet<Block> ghosts;
+    Block pacman;
+
+    //List of item and object positions
+    //  X = wall, O = skip, P = pac man, ' ' = food
+    //  Ghosts: b = blue, o = orange, p = pink, r = red
+    private String[] tileMap = {
+        "XXXXXXXXXXXXXXXXXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X                 X",
+        "X XX X XXXXX X XX X",
+        "X    X       X    X",
+        "XXXX XXXX XXXX XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXrXX X XXXX",
+        "O       bpo       O",
+        "XXXX X XXXXX X XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXXXX X XXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X  X     P     X  X",
+        "XX X X XXXXX X X XX",
+        "X    X   X   X    X",
+        "X XXXXXX X XXXXXX X",
+        "X                 X",
+        "XXXXXXXXXXXXXXXXXXX" 
+    };
 
     // Constructor 
     PacMan(){
@@ -41,4 +97,43 @@ public class PacMan extends JPanel{
 
     }
 
+    public void loadMap() {
+        walls = new HashSet<Block>();
+        foods = new HashSet<Block>();
+        ghosts = new HashSet<Block>();
+
+        //Loops through each row
+        for(int r = 0; r < rowCount; r++){
+            //Loops through each colum
+            for(int c = 0; c< columCount; c++){
+                String row = tileMap[r];
+                char tileMapChar = row.charAt(c);
+
+                int x = c * tileSize;
+                int y = r * tileSize;
+
+                if(tileMapChar == 'X'){
+                    Block wall = new Block(wallImage, x, y, tileSize, tileSize);
+                    walls.add(wall);
+                }
+                else if(tileMapChar == 'b'){
+                    Block ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                } 
+                else if(tileMapChar == 'p'){
+                    Block ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                }
+                else if(tileMapChar == 'r'){
+                    Block ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                }
+                else if(tileMapChar == 'o'){
+                    Block ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
+                    ghosts.add(ghost);
+                }
+            }
+        }
+
+    }
 }
