03a651ccccbe3007d0f687c932f0f17338790c7c
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-26 22:22:54 -0400

    Scoring and such

--- PackMan/src/PacMan.java
+++ PackMan/src/PacMan.java
@@ -98,6 +98,9 @@ public class PacMan extends JPanel implements ActionListener, KeyListener{
     Timer gameLoop;
     char[] directions = {'U', 'D', 'L', 'R'};
     Random random = new Random();
+    int score = 0;
+    int lives = 3;
+    boolean gameOver = false;
 
     //List of item and object positions
     //  X = wall, O = skip, P = pac man, ' ' = food
@@ -224,7 +227,13 @@ public class PacMan extends JPanel implements ActionListener, KeyListener{
             g.drawRect(food.x, food.y, food.width, food.height);
         }
 
-
+        g.setFont(new Font("Arial", Font.PLAIN, 18));
+        if(gameOver){
+            g.drawString("Game Over: " + String.valueOf(score), tileSize/2, tileSize/2);
+        }
+        else{
+            g.drawString("x" + String.valueOf(lives) + " Score: " + String.valueOf(score), tileSize/2, tileSize/2);
+        }
     }
 
     public void move(){
@@ -254,6 +263,15 @@ public class PacMan extends JPanel implements ActionListener, KeyListener{
                 }
             }
         }
+
+        Block foodEaten = null;
+        for (Block food : foods){
+            if(collision(pacman, food)){
+                foodEaten = food;
+                score +=10;
+            }
+        }
+        foods.remove(foodEaten);
     }
 
     public boolean collision(Block a, Block b){
